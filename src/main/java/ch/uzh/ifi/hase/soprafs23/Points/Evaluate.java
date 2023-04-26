c3c3c7454cca1b5508d54477373df16060d00c17
exostatistic <105790862+exostatistic@users.noreply.github.com>
2023-04-26 22:18:19 +0200

    resolves #51 and #95
    resolves #51 and #95

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Points/Evaluate.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Points/Evaluate.java
@@ -1,21 +1,99 @@
 package ch.uzh.ifi.hase.soprafs23.Points;
 
+import ch.uzh.ifi.hase.soprafs23.constant.CardColor;
 import ch.uzh.ifi.hase.soprafs23.constant.CardRank;
 import ch.uzh.ifi.hase.soprafs23.entity.Card;
 import ch.uzh.ifi.hase.soprafs23.entity.Player;
 
 import java.util.ArrayList;
 
-public class Evaluate {
+import static ch.uzh.ifi.hase.soprafs23.entity.Card.byRankComparator;
+
+public class Evaluate{
     private Player trickWinner;
 
     public void setTrickWinner(Player trickWinner) {
         this.trickWinner = trickWinner;
     }
 
-    public void setBonus() {
-        Trick trick = new Trick();
-        int bonus = 0;
+    public Player getTrickWinner(){
+        return trickWinner;
+    }
+
+    public Card check(Card card1, Card card2, CardColor trumpColor){
+        //both cards the same color returns the higher rank
+        if(card1.getColor() == card2.getColor()){
+            if(byRankComparator().compare(card1, card2) == 0){
+                return card1;
+            }
+            else return card2;
+        }
+        else if(card1.getColor() == trumpColor){
+            if(card2.getColor() == CardColor.SPECIAL){
+                if(card2.getaRank() == CardRank.ESCAPE)
+                    return card1;
+                else return card2;
+            }
+            else if(card2.getColor() == CardColor.BLACK){
+                return card2;
+            }
+            else return card1;
+        }
+        else if(card2.getColor() == trumpColor){
+            if(card1.getColor() == CardColor.SPECIAL){
+                if(card1.getaRank() == CardRank.ESCAPE)
+                    return card2;
+                else return card1;
+            }
+            else if(card1.getColor() == CardColor.BLACK){
+                return card1;
+            }
+            else return card2;
+        }
+        else if(card1.getColor() == CardColor.BLACK){
+            if(card2.getColor() == CardColor.SPECIAL){
+                if(byRankComparator().compare(card1, card2) == 0){
+                    return card1;
+                }
+                else return card2;
+            }
+            else return card1;
+        }
+        else if(card2.getColor() == CardColor.BLACK){
+            if(card1.getColor() == CardColor.SPECIAL){
+                if(byRankComparator().compare(card1, card2) == 0){
+                    return card1;
+                }
+                else return card2;
+            }
+            else return card2;
+        }
+        else if(byRankComparator().compare(card1, card2) == 0){
+                return card1;
+            }
+            else return card2;
+    }
+
+    public void compareCards(Trick trick){
+        ArrayList<Card> playedCards = (ArrayList<Card>) trick.getPlayedCards();
+        int l = playedCards.size();
+        Card highestCard = playedCards.get(0);
+        for (int i = 1; i < l; i++){
+            highestCard = check(highestCard, playedCards.get(i),trick.getTrumpColour());
+            if (highestCard.getaRank() == CardRank.SKULL_KING){
+                for(Card card : playedCards){
+                    if(card.getaRank() == CardRank.MERMAID){
+                        highestCard = card;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void calcBonus(Trick trick) {
+        int awardedBonus = 0;
         ArrayList<Card> playedCards = (ArrayList<Card>) trick.getPlayedCards();
         for (Card card : playedCards) {
             if (card.getaRank() == CardRank.SKULL_KING) {
@@ -23,17 +101,16 @@ public class Evaluate {
                     if (c.getaRank() == CardRank.PIRATE){
                         for (Card car : playedCards) {
                             if (car.getaRank() == CardRank.PIRATE) {
-                                bonus += 30;
+                                awardedBonus += 30;
                             }
                         }
                     }
                     else if (c.getaRank() == CardRank.MERMAID){
-                        bonus = 50;
+                        awardedBonus = 50;
                     }
-
                 }
             }
-            trickWinner.setBonus(bonus);
         }
+        getTrickWinner().setBonus(awardedBonus);
     }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Card.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Card.java
@@ -5,6 +5,7 @@ import ch.uzh.ifi.hase.soprafs23.constant.CardRank;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Card implements Serializable {
 
@@ -26,12 +27,12 @@ public class Card implements Serializable {
             if (color == CardColor.SPECIAL) {
                 //ESCAPE, PIRATE, MERMAID, SCARY_MARY, SKULL_KING
                 for (CardRank rank : CardRank.values()) {
-                    if(rank.ordinal() >= 13 && rank.ordinal() < 15){
+                    if(rank.ordinal() == 15 || rank.ordinal() == 0){
                         for(int i=0; i<5;i++){
                             CARDS.add(new Card(rank,color));
                         }
                     }
-                    if(rank.ordinal()==15){
+                    if(rank.ordinal()==14){
                         for(int i=0;i<2;i++){
                             CARDS.add(new Card(rank, color));
                         }
@@ -43,7 +44,10 @@ public class Card implements Serializable {
             }
             else{
                 for (CardRank rank : CardRank.values()) {
-                    if (rank == CardRank.ESCAPE) {
+                    if (rank == CardRank.ESCAPE){
+                        continue;
+                    }
+                    if (rank == CardRank.MERMAID) {
                         break;
                     }
                     CARDS.add(new Card(rank, color));
@@ -70,6 +74,10 @@ public class Card implements Serializable {
 
     public CardRank getaRank() {
         return this.aRank;
+    }
+
+    public static Comparator<Card> byRankComparator() {
+        return(card1, card2) -> card1.getaRank().compareTo(card2.getaRank());
     }
 
 }
