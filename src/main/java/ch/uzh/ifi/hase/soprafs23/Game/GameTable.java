777719b157b27f60b74f9982790f27a4042be258
Terebos <95576617+Terebos@users.noreply.github.com>
2023-04-26 23:18:15 +0200

    Update GameTable.java

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Game/GameTable.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Game/GameTable.java
@@ -15,18 +15,7 @@ public class GameTable implements Serializable {
     }
 
     public void addPlayer(Player p){
-        //test purpose
-        Player test = new Player();
-        test.setUsername("max");
-        test.setId(2L);
-        Order.add(test);
-        // right call
         Order.add(p);
-        //test purpose
-        Player test2 = new Player();
-        test2.setUsername("fritzz");
-        test2.setId(3L);
-        Order.add(test2);
     }
 
     public void setStartingPlayer(Player startingPlayer) {
