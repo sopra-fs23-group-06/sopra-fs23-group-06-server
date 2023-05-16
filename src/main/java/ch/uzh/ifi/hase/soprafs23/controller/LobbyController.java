99876300fa18fa2a9cc6dc82506a667356c9de01
LoreLadner <lorenzoladner@hotmail.com>
2023-05-16 23:34:27 +0200

    Added security Statements to GameSettings that throw COFLICT errors

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyController.java
@@ -81,6 +81,12 @@ public class LobbyController {
         if (playerInput.getId() != 1L) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Only the host can change the game settings");
         }
+        if (roundToEndGame > 10){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "The maximum of Rounds is 10 ");
+        }
+        if (maxPlayerSize <2){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "The minimum of Players are 2");
+        }
         lobbyService.gameSettings(lobbyCode,roundToEndGame,maxPlayerSize);
     }
 
